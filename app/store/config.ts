0fbce3038aa0b932b30ff22a0856de7579c6e90e
Wenhao Zhao <2867555086@qq.com>
2023-05-29 11:57:46 +0800

    更新默认enter键发送

--- app/store/config.ts
+++ app/store/config.ts
@@ -17,11 +17,11 @@ export enum Theme {
 }
 
 export const DEFAULT_CONFIG = {
-  submitKey: SubmitKey.CtrlEnter as SubmitKey,
+  submitKey: SubmitKey.Enter as SubmitKey,
   avatar: "1f603",
   fontSize: 14,
   theme: Theme.Auto as Theme,
-  tightBorder: false,
+  tightBorder: true,
   sendPreviewBubble: true,
   sidebarWidth: 300,
 
